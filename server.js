66c89a790cb0d3104e77d516d73fe6c2a194efdb
Eric <shantz.eric@gmail.com>
2023-08-19 00:00:26 -0400

    add other info to req.body

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express') //import express (node.js framework that makes setting up servers easy)
-const cors = require('cors') //import cors package
-const bcrypt = require("bcrypt")// import password hashing stuff
+const cors = require('cors'); //import cors package
+const bcrypt = require("bcrypt") //imports password hashing stuff
 const app = express() //create an instance of the express framework for us to use
 const port = process.env.PORT || 3001; //if port is configured use it, if not default to 3000
 
@@ -9,13 +9,14 @@ app.use(cors()); //use cors middleware
 app.use(express.json()); // tells express to make everything json in all routes
 
 
-//defines a route at root URL level
-app.post('/register', async(req, res) =>{
+//defines a route
+app.post('/register', async(req, res) => {
     // const {firstName, lastName, password, email} = req.body
-     const {password} = req.body
     try{
+        const {firstName, lastName, email, password} = req.body
         const hashedPassword = await bcrypt.hash(password, 10)
-        console.log(`PASSWORD ${hashedPassword}`)
+        console.log(hashedPassword);
+
         //TODO: create new user entry in db
         res.status(200).json({message: "User successfully registered!"})
     } catch (err){
@@ -24,7 +25,7 @@ app.post('/register', async(req, res) =>{
     }
 })
 
-app.get("/data", (req, res)=>{
+app.get("/user", (req, res)=>{
     const data = {message: "hello from the Back end!"}
     res.json(data) //use res.json instead of res.send so we dont have to set the content header
 })
