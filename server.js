b8323340db52df642f351ce4ae09e6e4acc87db1
Eric <shantz.eric@gmail.com>
2023-08-18 23:44:19 -0400

    add password hashing

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express') //import express (node.js framework that makes setting up servers easy)
 const cors = require('cors') //import cors package
+const bcrypt = require("bcrypt")// import password hashing stuff
 const app = express() //create an instance of the express framework for us to use
 const port = process.env.PORT || 3001; //if port is configured use it, if not default to 3000
+
+//Middleware
 app.use(cors()); //use cors middleware
+app.use(express.json()); // tells express to make everything json in all routes
 
 
 //defines a route at root URL level
-app.get('/', (req, res) =>{
-    res.send('Hello Express!')
+app.post('/register', async(req, res) =>{
+    // const {firstName, lastName, password, email} = req.body
+     const {password} = req.body
+    try{
+        const hashedPassword = await bcrypt.hash(password, 10)
+        console.log(`PASSWORD ${hashedPassword}`)
+        //TODO: create new user entry in db
+        res.status(200).json({message: "User successfully registered!"})
+    } catch (err){
+        console.error('Error hashing password:', err);
+        res.status(500).json({ message: 'An error occurred' });
+    }
 })
 
 app.get("/data", (req, res)=>{
