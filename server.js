8ddb3ec6479d7791abd6119de542c8b0da63a90f
Eric <shantz.eric@gmail.com>
2023-08-22 15:52:43 -0400

    create DB, register endpoint creates a new user

--- server.js
+++ server.js
@@ -3,21 +3,54 @@ const cors = require('cors'); //import cors package
 const bcrypt = require("bcrypt") //imports password hashing stuff
 const app = express() //create an instance of the express framework for us to use
 const port = process.env.PORT || 3001; //if port is configured use it, if not default to 3000
+const mysql = require("mysql2")
 
 //Middleware
 app.use(cors()); //use cors middleware
 app.use(express.json()); // tells express to make everything json in all routes
 
+const dbConnection = mysql.createConnection({
+    host: 'sql9.freemysqlhosting.net',
+    user: 'sql9641671',
+    password: 'vJ1gyPXkHx',
+    database: 'sql9641671' 
+})
+
+dbConnection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      return;
+    }
+    console.log('Connected to the database!');
+  });
+
 
 //defines a route
 app.post('/register', async(req, res) => {
-    // const {firstName, lastName, password, email} = req.body
     try{
+
+        //TODO: check if email already exists in DB, if so, throw error with new status to be used in the front end
+
         const {firstName, lastName, email, password} = req.body
         const hashedPassword = await bcrypt.hash(password, 10)
-        console.log(hashedPassword);
 
         //TODO: create new user entry in db
+        const newUser = {
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            password: hashedPassword
+        }
+
+        dbConnection.query("INSERT INTO users SET ?", newUser, (error, results) => {
+            if (error) {
+                console.error('Error inserting user:', error);
+              } else {
+                console.log('User inserted successfully');
+            }
+        }
+        )
+
         res.status(200).json({message: "User successfully registered!"})
     } catch (err){
         console.error('Error hashing password:', err);
